d1ef8cc5b8c02d977b02da441da7f8806f6dfa84
Wizardbrew <wizardbrew84@gmail.com>
2025-06-26 04:35:08 +0530

    Test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
         public static void main(String args[]){
-        System.out.println("Hello World!");
+            System.out.println("Hello World!");
+        System.out.print("Parvez Your First Java Program is working");
     }
 }
